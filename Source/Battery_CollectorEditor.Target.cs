653d814425742ae4dbdb4aea0c56c13181ed7538
Nicole Tilbe <nicolecali2406@outlook.com>
2020-03-18 22:00:02 -0400

    Initial commit

---
+++ Source/Battery_CollectorEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Battery_CollectorEditorTarget : TargetRules
+{
+	public Battery_CollectorEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("Battery_Collector");
+	}
+}
